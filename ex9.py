ef8c2e5720c9b5e6c2e4e2267deb9ff4444d304e
shway250 <jackson_duhon@yahoo.com>
2018-02-11 23:41:13 -0800

    multi line strings

---
+++ ex9.py
@@ -0,0 +1,12 @@
+#more string stuff
+days = "Mon Tues Wed Thurs Fri Sat Sun"
+months = "\nJan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("here are the days", days)
+print("here are the months:", months)
+
+print("""
+multi line strings
+are pretty awesome
+this is so easy
+without having to use the new line thingy """)
