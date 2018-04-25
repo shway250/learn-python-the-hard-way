8e05713587ec149397a7baf953b7eea6403f156a
shway250 <jackson_duhon@yahoo.com>
2018-04-25 14:00:16 -0700

    getting user input

---
+++ ex11.py
@@ -0,0 +1,12 @@
+print("how old are you?", end=' ')
+age = input()
+print("how tall are you?", end=' ')
+height = input()
+print("how fat are you?", end=' ')
+weight = input()
+
+print(f"So, you're {age} old, {height} tall, and {weight} heavy.", end = ' ')
+
+print("now enter a number!")
+x = int(input())
+print(x)
