19106f21bda783c1460da242ea8b537fcdf68559
shway250 <jackson_duhon@yahoo.com>
2018-04-25 21:48:39 -0700

    argv module

---
+++ ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, first, second, third = argv
+
+print("the script is called:", script)
+print("the first variable is called:", first)
+print("the second variable is called:", second)
+print("the third variable is called:", third)
