2e10d228a6a8d374005ae42e3f64ee82c57c54cd
shway250 <jackson_duhon@yahoo.com>
2018-04-25 21:21:37 -0700

    user prompt

---
+++ ex12.py
@@ -0,0 +1,5 @@
+blergle = input("Hey, what's a blergle?")
+farge = input("What did you say about my mom?!")
+last_variable = input("GAAAAAH MYE EYES!!!")
+
+print(f"A blergle is a {blergle}, and your mom {farge}, GAAAAAH MY {last_variable}")
